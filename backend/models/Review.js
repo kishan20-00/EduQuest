3472f4e8ed78b4513addbe8f22a3e64e612f64d3
Kishan Raj <86339947+kishan20-00@users.noreply.github.com>
2024-10-27 17:50:31 +0530

    Updated Review.js

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -2,8 +2,8 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Content', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'EduUsers', required: true },
+  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
   rating: { type: Number, min: 1, max: 5, required: true },
   comment: { type: String, required: true },
 }, { timestamps: true });
